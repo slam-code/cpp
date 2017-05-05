71eac2ccb75fe0085c8bbd722dbb719ad0a81253
learnmoreonce <ushendaiyo@163.com>
2017-05-05 23:42:55 +0800

    0.1

--- Cpp-Concurrency-in-Action/listing_3.12.cpp
+++ Cpp-Concurrency-in-Action/listing_3.12.cpp
@@ -26,7 +26,7 @@ struct remote_connection_manager
 } connection_manager;
 
 
-class X
+class X   //使用std::call_once作为类成员的延迟初始化(线程安全)
 {
 private:
     connection_info connection_details;
@@ -43,7 +43,8 @@ public:
     {}
     void send_data(data_packet const& data)
     {
-        std::call_once(connection_init_flag,&X::open_connection,this);//使用open_connection函数完成初始化数据
+        std::call_once(connection_init_flag,&X::open_connection,this);
+        //使用open_connection函数完成初始化数据
         connection.send_data(data);
     }
     data_packet receive_data()
@@ -53,14 +54,27 @@ public:
     }
 };
 
-
+/*
+在这个例子中,第一个调用send_data()或receive_data()的线程完成初始化过程。使用成
+员函数open_connection()去初始化数据,也需要将this指针传进去。和其在标准库中的函数
+一样,其接受可调用对象,比如 	std::thread	的构造函数和 std::bind(),
+通过向std::call_once()传递一个额外的参数来完成这个操作。
+值得注意的是,	std::mutex和std::one_flag的实例就不能拷贝和移动,所以当你使用它们
+作为类成员函数,如果你需要用到他们,你就得显示定义这些特殊的成员函数。
+ */
 class my_class{};
 my_class& get_my_class_instance()//局部变量声明为static且以后不更新：c++11后，初始化与定义均在一个线程中发生，是线程安全的初始化。
 {
     static my_class instance;
     return instance;
 }
-
+/*其中一个局部变量被声明为static类型。这种变量的在声明后就已经完成初始化;
+ * 对于多线程调用的函数,这就意味着这里有条件竞争——抢着去定义这个变量。
+ * 在很多不支持C++11标准的编译器中,这样的条件竞争是确实存在的,因为在多线程中,
+ * 每个线程都认为他们是第一个初始化这个变量线程;或一个线程对变量进行初始化,
+ * 而另外一个线程要使用这个变量时,初始化过程还没完成。在C++11标准中,这些问题都被解决了:
+ * 初始化及定义完全在一个线程中发生,并且没有其他线程可在初始化完成前对其进行处理
+*/
  //安全初始化，但极少更新：见3.13。
 
 int main()
--- Cpp-Concurrency-in-Action/listing_3.13.cpp
+++ Cpp-Concurrency-in-Action/listing_3.13.cpp
@@ -9,10 +9,12 @@ class dns_entry
 /*http://www.linuxidc.com/Linux/2011-07/39215p11.htm
  * http://blog.csdn.net/jongden/article/details/25800365
 shared_lock是read lock。被锁后仍允许其他线程执行同样被shared_lock的代码。这是一般做读操作时的需要。
-unique_lock是write lock。被锁后不允许其他线程执行被shared_lock或unique_lock的代码。在写操作时，
- 一般用这个，可以同时限制unique_lock的写和share_lock的读。
+
+unique_lock是write lock。被锁后不允许其他线程执行被shared_lock或unique_lock的代码。
+ 在写操作时， 一般用这个，可以同时限制unique_lock的写和share_lock的读。
  boost::shared_lock 类型的非独占锁可以在线程只对某个资源读访问的情况下使用。一个线程修改的资源需要写访问，
- 因此需要一个独占锁。 这样做也很明显：只需要读访问的线程不需要知道同一时间其他线程是否访问。 因此非独占锁可以共享一个互斥体。
+ 因此需要一个独占锁。 这样做也很明显：只需要读访问的线程不需要知道同一时间其他线程是否访问。
+ 因此非独占锁可以共享一个互斥体。
  * */
 class dns_cache
 {
@@ -35,7 +37,40 @@ public:
     }
 };
 
-int main()
+int main()//保护很少更新的数据结构
 {
     std::cout<<"3.13 boost ok ";
 }
+
+/*http://en.cppreference.com/w/cpp/thread/shared_lock
+ * 	std::mutex来保护数据结构,这的确有些反应过度,
+ * 这里需要另一种不同的互斥量。这种新的互斥量常被称为“读者-写者锁”(reader-writer	mutex),
+ * 因为其允许两中不同的使用方式:一个“作者”线程独占访问和共享访问,让多个“读者”线程并发访问。
+ * */
+
+/*比起使用std::mutex实例进行同步,不如使用boost::shared_mutex来做同步。对于更新操作,可以使
+用std::lock_guard<boost::shared_mutex>和std::unique_lock<boost::shared_mutex>进行上锁,
+ 作为std::mutex的替代方案。与std::mutex所做的一样,这就能保证更新线程的独占访问。
+ 其他线程不需要去修改数据结构,其实现可以使用boost::shared_lock<boost::shared_mutex>获取共享访问权。
+ 这与使用	std::unique_lock一样,除非多线要在同时得到同一个 	boost::shared_mutex 上有共享锁。
+ 唯一的限制就是,当任一线程拥有一个共享锁时,这个线程就会尝试获取一个独占锁,直到其他线程放弃他们的
+锁;同样的,当任一线程拥有一个独占锁是,其他线程就无法获得共享锁或独占锁,直到第
+一个线程放弃其拥有的锁。*/
+
+/*
+在清单3.13中,find_entry()使用了 	boost::shared_lock<>实例来保护其共享和只读权限;这
+就使得,多线程可以同时调用find_entry(),且不会出错。
+
+另一方面,update_or_add_entry()使用std::lock_guard<>对象,当表格需要更新时,为其提供独占访问权限;
+在update_or_add_entry()函数调用时,独占锁会阻止其他线程对数据结构进行修改,
+并且这些线程在这时,也不能调用find_entry()。
+ */
+
+/*
+简单的说：
+
+shared_lock是read lock。被锁后仍允许其他线程执行同样被shared_lock的代码。这是一般做读操作时的需要。
+
+unique_lock是write lock。被锁后不允许其他线程执行被shared_lock或unique_lock的代码。
+在写操作时，一般用这个，可以同时限制unique_lock的写和share_lock的读。
+ */
